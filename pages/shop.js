bd9e7133c0da1c5c4792e98397fca3a6120c05f8
mohammad2174 <mohammadreza.khorrami21@gmail.com>
2022-06-12 11:41:37 +0430

    connected links to categories

--- pages/shop.js
+++ pages/shop.js
@@ -1,33 +1,3 @@
-// import React from 'react'
-// import Link from 'next/link';
-// import { useRouter } from 'next/router'
-// import { fetchCollectionStartAsync } from '../redux/shop/shop.action'
-// import CollectionPageContainer from '../components/CollectionContainer';
-// import CollectionOverViewContainer from '../components/CollectionOverviewContainer'
-// import { connect } from 'react-redux'
-
-// class ShopPage extends React.Component {
-//     componentDidMount() {
-//         const { fetchCollectionStartAsync } = this.props
-//         fetchCollectionStartAsync()
-//     }
-
-//     render() {
-//         const { match } = this.props
-//         return (
-//             <div className='shop-page'>
-//                 <Link exact path={`${match.path}`}>{CollectionOverViewContainer}</Link>
-//                 <Link path={`${match.path}/:collectionId`}>{CollectionPageContainer}</Link>
-//             </div>
-//         )
-//     }
-// }
-
-// const mapDispatchToProps = dispatch => ({
-//     fetchCollectionStartAsync: () => dispatch(fetchCollectionStartAsync())
-// })
-
-// export default connect(null, mapDispatchToProps)(ShopPage)
 import { Component } from 'react';
 import SHOP_DATA from './shop.data';
 import CollectionPreview from '../components/CollectionPreview';
